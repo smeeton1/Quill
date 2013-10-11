c961f9fe142ad509b855dd94a1d8221a9eae40bf
smeeton <smeeton@105ee1b7-925d-4305-98bb-6b743c1a0f71>
2013-10-11 08:14:46 +0000

    latest version with matrix exponianal added need to work on utillity functions

---
+++ src/Densutil.F90
@@ -0,0 +1,56 @@
+module Densutil
+
+!some function for working with density matirices.
+!all functions are assuming that the density matrix rho is in the vectorised form of lenght n*n and that the colomes follow each other in order.
+
+ Integer, parameter   :: kdp = selected_real_kind(15)
+ private              :: kdp
+ contains
+
+ !gets the norm of the density matrix Tr(rho)
+ complex(kdp) function get_Norm(rho) (norm)
+  complex(kdp),dimension(:),intent(in):: rho
+  integer                             :: n, sqn, i
+  
+  n=size(rho)
+  sqn=sqrt(n)
+
+  norm=rho(1)
+
+  do i=2,n
+    if(i.eq.())then  
+      norm=norm+rho(i)
+    endif
+  enddo
+
+
+ end function
+
+ !writes out the density matrix
+ subroutine write_rho(rho, filename)
+  complex(kdp),dimension(:),intent(in):: rho
+  character(len=80),intent(in)        :: filename 
+  integer                             :: n, i,j
+
+  n=sqrt(size(rho))
+
+  open(4,file=filename,STATUS='append',ACTION='write')
+
+  
+  do i=1,n
+   do j=0,n-1
+       write(4,"(F7.3,a,a,F7.3,a)")real(rho(i+j*n)),' ','i',aimag(rho(i+j*n)),' '
+   enddo 
+   write(4,*)'\n'
+  enddo
+  close(4)
+
+ end subroutine
+
+ !gets the pointer states from the density matrix
+ !assumes these are the states along the diagonal
+ subroutine extract_pointerS(rho, point_state)
+
+ end subroutine
+
+end module
