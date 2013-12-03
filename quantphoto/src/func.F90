59efe9c7cec2ac6ba1a19bb716339e5a37d8da5b
smeeton <smeeton@105ee1b7-925d-4305-98bb-6b743c1a0f71>
2013-12-03 06:03:59 +0000

    adding a file for basic math functions

---
+++ quantphoto/src/func.F90
@@ -0,0 +1,22 @@
+module solver
+use eigenSolve
+use until
+!use special
+implicit none
+
+
+ Integer, parameter   :: kdp = selected_real_kind(15)
+ private              :: kdp
+
+ contains
+ 
+ subroutine first_moment()
+ 
+ end subroutine
+ 
+ subroutine second_moment()
+ 
+ end subroutine
+ 
+ 
+end module
