1c8bea2c99ce6249ec3ac10fa173454cb0516ecf
smeeton <smeeton@105ee1b7-925d-4305-98bb-6b743c1a0f71>
2014-03-17 08:14:30 +0000

    fixed quantum con function need fix class

--- src/3edge.F90
+++ src/3edge.F90
@@ -36,7 +36,7 @@ program photest
 !   alpha=0.1
   allocate(D(n,n),rho(n*n),p(n))
 
-  v=.false.
+  v=.true.
   r=.true.
 ! write(*,*)size(D,1)
   call Read_MatR(infile,D)
@@ -46,26 +46,33 @@ program photest
   do j=1,k
     write(dirname,'(a,I5.5)')trim(dirnamebase),j 
     call make_dir(dirname)
-    if(j.ne.1)then
-     call Rand_Weight(D)
-    endif
+!     if(j.ne.1)then
+!      call Rand_Weight(D)
+!     endif
     write(filename,'(3a)')trim(dirname),'/','D'
     call write_Mat(filename,D)
     rho(:)=cmplx(0,0);!rho(1)=cmplx(1./3.,0);rho(5)=cmplx(1./3.,0);rho(9)=cmplx(1./3.,0)
     do i=1,n
       rho(i+(i-1)*n)=cmplx(1./n,0.0)
     enddo
-!  call write_Mat('D',D) 
-
+    write(filename,'(3a)')trim(dirname),'/',trim(filenamebase)
     open(4,file=filename,STATUS='unknown',ACCESS='append',ACTION='write')
+    write(4,*)' '
+    write(4,*)'------------------------------------------------------------------------------'
     write(4,*)'itarive quantum non row normalized'
     close(4)
     do l=1,10
+        rho(:)=cmplx(0,0);!rho(1)=cmplx(1./3.,0);rho(5)=cmplx(1./3.,0);rho(9)=cmplx(1./3.,0)
+      do i=1,n
+         rho(i+(i-1)*n)=cmplx(1./n,0.0)
+      enddo
       alpha=0.1*real(l)
       open(4,file=filename,STATUS='unknown',ACCESS='append',ACTION='write')
+      write(4,*)' '
+      write(4,*)'******************************************************************************'
       write(4,*)'alpha = ',alpha
       close(4)
-      call Dir_Gra_Run(D, rho, t, alpha, filename, v, r)
+      call Dir_Gra_Con(D, rho, err, alpha, filename, r)
     enddo
         
     call row_norm(D)
@@ -78,33 +85,49 @@ program photest
     
     
     open(4,file=filename,STATUS='unknown',ACCESS='append',ACTION='write')
+    write(4,*)' '
+    write(4,*)'------------------------------------------------------------------------------'
     write(4,*)'itarive quantum row normalized'
     close(4)
     do l=1,10
+        rho(:)=cmplx(0,0);!rho(1)=cmplx(1./3.,0);rho(5)=cmplx(1./3.,0);rho(9)=cmplx(1./3.,0)
+        do i=1,n
+          rho(i+(i-1)*n)=cmplx(1./n,0.0)
+        enddo
       alpha=0.1*real(l)
       open(4,file=filename,STATUS='unknown',ACCESS='append',ACTION='write')
+      write(4,*)' '
+      write(4,*)'******************************************************************************'
       write(4,*)'alpha = ',alpha
       close(4)
       call Dir_Gra_Con(D, rho, err, alpha, filename, r)
     enddo
     
     open(4,file=filename,STATUS='unknown',ACCESS='append',ACTION='write')
+    write(4,*)' '
+    write(4,*)'------------------------------------------------------------------------------'
     write(4,*)'itarive class'
     close(4)
     do l=1,10
       alpha=0.1*real(l)
       open(4,file=filename,STATUS='unknown',ACCESS='append',ACTION='write')
+      write(4,*)' '
+      write(4,*)'******************************************************************************'
       write(4,*)'alpha = ',alpha
       close(4)
       call pagerank_it(D, p, err, alpha, filename)
     enddo
        
     open(4,file=filename,STATUS='unknown',ACCESS='append',ACTION='write')
+    write(4,*)' '
+    write(4,*)'------------------------------------------------------------------------------'
     write(4,*)'eigenvalue class'
     close(4)
     do l=1,10
       alpha=0.1*real(l)
       open(4,file=filename,STATUS='unknown',ACCESS='append',ACTION='write')
+      write(4,*)' '
+      write(4,*)'******************************************************************************'
       write(4,*)'alpha = ',alpha
       close(4)
       call pagerank_ei(D, p, alpha, filename, work)
@@ -114,15 +137,22 @@ program photest
 
 
   
-!   ! graph 1
+  ! graph 1
 !   filename='graph_1_1'
-!   D(1,1)=0.0;D(1,2)=1.;D(1,3)=4.
-!   D(2,1)=1./2.;D(2,2)=0.0;D(2,3)=1./2.
-!   D(3,1)=0.0;D(3,2)=1.;D(3,3)=0.0
+!   D(1,1)=0.0;D(1,2)=1.;D(1,3)=1.
+!   D(2,1)=0.0;D(2,2)=0.0;D(2,3)=1.
+!   D(3,1)=1.0;D(3,2)=0.;D(3,3)=0.0
 !   rho(:)=cmplx(0,0);rho(1)=cmplx(1./3.,0);rho(5)=cmplx(1./3.,0);rho(9)=cmplx(1./3.,0)
 !   write(*,*)filename
-!   call Dir_Gra_Run(D, rho, t, alpha, filename, v, r)
-!   
+!  do l=1,10
+!    rho(:)=cmplx(0,0);rho(1)=cmplx(1./3.,0);rho(5)=cmplx(1./3.,0);rho(9)=cmplx(1./3.,0)
+!   alpha=0.1*real(l)
+!       open(4,file=filename,STATUS='unknown',ACCESS='append',ACTION='write')
+!       write(4,*)'alpha = ',alpha
+!       close(4)
+!    call Dir_Gra_Run(D, rho, t, alpha, filename, v, r)
+!  enddo
+! !   
 !   ! graph 2
 !   filename='graph_2'
 !   D(1,1)=0.0;D(1,2)=0.0;D(1,3)=1.
