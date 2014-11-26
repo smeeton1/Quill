22dafd795e3c25070cd97ca66d01e8afe1b7e608
smeeton <smeeton@105ee1b7-925d-4305-98bb-6b743c1a0f71>
2014-11-26 08:45:48 +0000

    added file

---
+++ quantphoto/src/directwalk.F90
@@ -0,0 +1,19 @@
+module Directwalk
+use eigenSolve
+use until
+use func
+use Densutil
+use entangle
+use supperoperator
+!use special
+implicit none
+
+
+ Integer, parameter   :: kdp = selected_real_kind(15)
+ private              :: kdp
+
+ contains
+ 
+ 
+ 
+ end module
