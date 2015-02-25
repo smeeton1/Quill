d69308b0ab06e14821cf94ed5fe76dda1d18fa60
smeeton <smeeton@105ee1b7-925d-4305-98bb-6b743c1a0f71>
2015-02-25 05:58:52 +0000

    added some graphs and started a new modue for discreet

--- quantphoto/src/directwalk.F90
+++ quantphoto/src/directwalk.F90
@@ -90,89 +90,7 @@ implicit none
   deallocate(H,D2w,H2w)
 
  end subroutine
- 
- !makes the coin operator for a 8 node line
- subroutine L_make_DCDG(SO,alpha)
-  complex(kdp), dimension(:,:), intent(inout)  :: SO
-  real(kdp),intent(in)                         :: alpha
-  integer                                      :: i,j,k,l,n
-  complex(kdp), dimension(:,:), allocatable    :: H,D
-  real(kdp)                                    :: sca
 
-  n=16 !size(D,1)
-  allocate(H(n,n),D(n,n))
-  
-  sca=1.0
-  H(:,:)=cmplx(0,0)
-  D(:,:)=cmplx(0,0)
-
-  do i=1,n
-    if(mod(i,2).eq.0)then
-       H(i,i)=-1*sca
-       H(i,i-1)=1*sca
-    else
-       H(i,i)=1*sca
-       H(i,i+1)=1*sca
-    endif
-  enddo
-  
-  do i=1,n
-    if(mod(i,2).eq.0)then
-       D(i,i)=-1*sca
-    else
-       D(i,i+1)=1*sca
-    endif
-  enddo
-
-
- 
-  call B_SO_for_H((1-alpha)*H,SO)
-   
-  call Stand_to_SO(alpha*D,SO)
- 
-
-  deallocate(H,D)
-
- end subroutine
- 
- !makes the transition operator for a 8 node line
- subroutine L_make_DTDG(SO,alpha)
-  complex(kdp), dimension(:,:), intent(inout)  :: SO
-  real(kdp),intent(in)                         :: alpha
-  integer                                      :: i,j,k,l,n
-  complex(kdp), dimension(:,:), allocatable    :: H, D
-  real(kdp)                                    :: sca
-
-  n=16!size(D,1)
-  allocate(H(n,n),D(n,n))
-  
-  sca=1.0
-  H(:,:)=cmplx(0,0)
-  D(:,:)=cmplx(0,0)
-
-  
-  H(1,15)=1
-  do i=2,n
-    if(mod(i,2).eq.0)then
-       H(i,i+2)=1*sca
-    else
-       H(i,i-2)=1*sca
-    endif
-  enddo
-  
-  do i=1,n
-    if(mod(i,2).eq.0)then
-       D(i,i+2)=1*sca
-    endif
-  enddo
-  call write_Mat_real('H',H)
- 
-  call B_SO_for_H((1-alpha)*H,SO)    
-  call Stand_to_SO(alpha*D,SO)
-
-  deallocate(H,D)
-
- end subroutine
  
  
  ! performs a walk on a directed graph using open system methods and dose the walk from time 1 to t
@@ -310,63 +228,7 @@ implicit none
   deallocate(SO,rho_out,psi)
 
  end subroutine
- 
- 
- !Performs a directional walk for a discreet walker on an 8 node line
- subroutine Dis_Dir_8line(rho, err, alpha, beta, filename, r)
-  complex(kdp), dimension(:), intent(inout)    :: rho
-  real(kdp), intent(in)                        :: alpha,beta,err
-  logical, intent(in)                          :: r
-  character(len=80),intent(in)                 :: filename
-  real(kdp)                                    :: error
-  integer                                      :: i,n
-  complex(kdp), dimension(:,:), allocatable    :: SOC, SOT, psi
-  complex(kdp)                                 :: norm
-  complex(kdp), dimension(:), allocatable      :: rho_out  
-  character(len=90)                            :: filename2
-  
-  n=16
-  allocate(SOC(n*n,n*n),SOT(n*n,n*n),rho_out(n*n),psi(2,n))
-  
-  SOC(:,:)=cmplx(0,0)
-  SOT(:,:)=cmplx(0,0)
-  call L_make_DCDG(SOC,alpha)
-  call L_make_DTDG(SOT,beta)
-  
- ! call write_Mat('SO',SO)
-  call extract_pointerS(rho, psi(1,:))
- ! write(*,*)size(rho),size(SOC,1),size(SOT,1)
-  i=1
-  error=1
-  rho_out=rho
-  do while ((error.gt.err).or.(i.gt.10000))
-    i=i+1
-    rho_out= matmul(SOC,rho_out)
-    rho_out= matmul(SOT,rho_out)
-    call extract_pointerS(rho_out, psi(2,1:n))
-    error = maxval(abs(psi(1,1:n)-psi(2,1:n)))
-    psi(1,:)=psi(2,:)
-  enddo
-  if(r)then
-    call write_Mat_real(filename,psi)
-  else
-    call write_Mat(filename,psi)
-  endif
-  
-  !call write_moments(filename,psi)
-  
 
-  call write_Vec(filename,psi(2,1:n))
-  norm= get_Norm(rho_out)
-  open(4,file=filename,STATUS='unknown',ACCESS='append',ACTION='write')
-  write(4,*)''
-  write(4,"(a,2F7.3)")' norm= ',norm
-  close(4)
-  rho=rho_out
-  
-  deallocate(SOC,SOT,rho_out,psi)
-
- end subroutine
  
  subroutine Dir_2walker(D, rho1,rho2, t, alpha, filename, interact)
   complex(kdp), dimension(:,:), intent(in)     :: D
